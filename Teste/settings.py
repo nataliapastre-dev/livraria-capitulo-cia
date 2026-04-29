df3d80550c32ed58678d09a1220218e39fefbfe4
nataliapastre-dev <natalia.pastre10@gmail.com>
2026-04-29 20:53:14 -0300

    adicionando whitenoise

--- Teste/settings.py
+++ Teste/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,9 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL =  'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_DIRS = [
+ #   BASE_DIR / "static"
+#]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
